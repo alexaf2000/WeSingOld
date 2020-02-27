e4bc007054a43d51674835173ca6f24e1e95d3e4
Alex A.F <alex2andres@gmail.com>
2020-02-27 20:14:37 +0100

    Add glad (openGL)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <glad/glad.h>
 #include <glfw/glfw3.h>
+
 #include <iostream>
 
 int main(void)
@@ -8,7 +10,9 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1280, 720, "WeSing", NULL, NULL);
     if (!window)
@@ -17,13 +21,21 @@ int main(void)
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
