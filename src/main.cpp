88fbdb94613fff1dca8a267296c8676f42b8c7c7
Alex A.F <alex2andres@gmail.com>
2020-03-05 21:40:49 +0100

    Set src folder

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <inipp.h>
+
+#include <iostream>
+
+#include <fstream>
+
+int main(void)
+{
+    GLFWwindow *window;
+    std::cout << "Test" << std::endl;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1920, 1080, "WeSing", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+		glClear(GL_COLOR_BUFFER_BIT);
+		float vertices[] = {
+	  -0.5f, -0.5f, 0.0f,
+	   0.5f, -0.5f, 0.0f,
+	   0.0f,  0.5f, 0.0f
+		};
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+        //glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
